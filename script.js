cfdfcc2ace704c458d2d798c7014eb78087711de
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2023-07-03 09:59:47 -0400

    javascript functions written for redirecting and passing email to success page

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ let emailForm = document.getElementsByClassName("email-form")[0];
 emailForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let userEmail = document.getElementById("email").value;
+  let userEmail = document.getElementById("emailAddress").value;
 
   const isValid = isValidEmail(userEmail);
 
   if (!isValid) {
+    let errorText = document.getElementById("errorText");
+    errorText.classList.remove("error-text-disabled");
+    errorText.classList.add("error-text");
 
+    const emailInput = document.getElementById("emailAddress");
+    emailInput.classList.add("error-input");
+    emailInput.value = "";
+  } else if (errorText.classList.contains("error-text") || isValid) {
+    errorText.classList.remove("error-text");
+    errorText.classList.add("error-text-disabled");
+    // emailInput.classList.remove("error-input");
+
+    localStorage.setItem("email", userEmail);
+    window.location.href = "success.html";
   }
 });
 
@@ -15,4 +28,5 @@ function isValidEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
+
 
